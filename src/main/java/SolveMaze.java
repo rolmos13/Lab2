ce48dcd214ef069cecdd3452abfded208e6c104f
rigol <rolmos2@illinois.edu>
2018-01-30 12:02:25 -0600

    Lab21

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,14 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            while(maze.isFinished()==false){
+            maze.turnRight();
+            while (maze.canMove()==false){
+                maze.turnLeft();
+            }
+            maze.move();
+        }
+
             // Implement your maze solving algorithm here
         }
 
